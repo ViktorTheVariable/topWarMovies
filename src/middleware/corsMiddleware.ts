f656b0ac1539d4a159ad10e7ebee4f0cd62fdac5
Viktor Ekstrom <viktor.ekstrom@chasacademy.se>
2025-03-23 15:18:02 +0100

    moved CORS logic to a new file in middleware folder, ,odified logic for security

---
+++ src/middleware/corsMiddleware.ts
@@ -0,0 +1,17 @@
+import cors from 'cors';
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
+
+const corsMiddleware = cors({
+    origin: (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) => {
+        if (origin && allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+    });
+
+export default corsMiddleware;
